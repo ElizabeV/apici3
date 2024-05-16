61e411f49b5bd64db091e773b1db33cf0eddcacc
Elizabeth Viricheva <liza.vir@ya.ru>
2024-05-16 20:48:05 +0300

    success

---
+++ src/test/java/ru/netology/RequestPostTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RequestPostTest {
+    @Test
+    void shouldReturnDataFromBody () {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("ping")
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("ping"))
+        ;
+    }
+}
